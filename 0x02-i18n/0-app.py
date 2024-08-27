b76c7b1e56377887a38d311af588b83b38b2bf09
kelvinnede <kelvinnede@gmail.com>
2024-08-27 03:21:56 -0700

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app for displaying a welcome message.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Route for the index page.
+    Renders a simple HTML template with a welcome message.
+    """
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run()
