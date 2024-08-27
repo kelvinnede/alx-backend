3080bc64dd818a473e8e87995a47b5826261412a
kelvinnede <kelvinnede@gmail.com>
2024-08-27 03:24:15 -0700

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
@@ -14,6 +15,7 @@ def index():
     Renders a simple HTML template with a welcome message.
     """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run()
